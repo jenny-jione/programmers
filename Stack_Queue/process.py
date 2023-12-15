f79456fdf18aeb62f44e5d079c17123434c85f73
jenny.jione.jang <jenny.jione.jang@gmail.com>
2023-12-15 17:41:00 +0900

    (ing) [스택/큐] 프로세스

---
+++ Stack_Queue/process.py
@@ -0,0 +1,35 @@
+"""
+문제 이름: 프로세스
+문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/42587?language=python3
+"""
+
+# 2, 5, 18 틀림
+
+def solution(priorities, location):
+    answer = 0
+    # 관건: location의 프로세스를 어떻게 추적하는지
+    # tuple?
+    prior = []
+    loc = []
+    for i, p in enumerate(priorities):
+        prior.append(p)
+        loc.append(i==location)
+
+    count = 0
+    while(prior):
+        if len(prior) == 1:
+            break
+        cur_p = prior[0]
+        cur_l = loc[0]
+        if cur_p >= max(prior[1:]):
+            prior = prior[1:]
+            loc = loc[1:]
+            count += 1
+            if cur_l == True:
+                answer = count
+                break
+        else:
+            prior = prior[1:] + [cur_p]
+            loc = loc[1:] + [cur_l]
+        
+    return answer
